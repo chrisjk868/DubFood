7d9f215afdf2a2cc3d5ecc298e54633d8a491ce3
amayakejriwal <amayak@uw.edu>
2022-06-05 11:57:30 -0700

    added reading local files to user profile

--- DubFood/UserProfileViewController.swift
+++ DubFood/UserProfileViewController.swift
@@ -6,6 +6,11 @@
 //
 
 import UIKit
+import Foundation
+
+struct UserData: Codable {
+    let username, email, university: String
+}
 
 class UserProfileViewController: UIViewController {
     
@@ -14,10 +19,32 @@ class UserProfileViewController: UIViewController {
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var universityLabel: UILabel!
     
+    var userInfo : [UserData] = []
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        showUserInfo()
         // Do any additional setup after loading the view.
+    }
+    
+    func showUserInfo() {
+        // get data from local files
+        print("getting user data from local files")
+        if let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let pathWithFilename = documentDirectory.appendingPathComponent("userInfo.json")
+            do {
+                let data = try Data(contentsOf: pathWithFilename)
+                let info = try JSONDecoder().decode(UserData.self, from: data)
+                self.userInfo = [info]
+            } catch {
+                print("There was an error getting user data from the local file: \(error)")
+            }
+        }
+        
+        print("self.userInfo: \(self.userInfo)")
     }
     
 
